81cd51ed356018d6456ace2e0d1f8f3ff4c24a27
vikash <zero_transfer@outlook.com>
2019-09-18 14:37:43 +0100

    step 3

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+# first, we print the list of students
 puts "The students of villain's academy"
 puts "____________"
 puts "Dr. Hannibal Lecter"
@@ -11,8 +13,6 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
-print "Overall, we have "
-
-print 11
-puts " great students"
+#finally, we print the total number of students
+puts "Overall, we have #{student_count} great students"
+# it's important that prince() doesn't add new line character
