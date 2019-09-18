818d078dcb651a3a1994b052e92005da12b905b9
vikash <zero_transfer@outlook.com>
2019-09-18 15:17:34 +0100

    step7

--- directory.rb
+++ directory.rb
@@ -15,6 +15,20 @@ students = [
 {name: "Norman Bates", cohort: :november},
 ]
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish. just git return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+students
+end
+
+
 def print_header()
   puts "The students of Villains Academy"
   puts "____________"
@@ -29,6 +43,8 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
-print_header
+
+students = input_students
+print_header()
 print(students)
 print_footer(students)
