c5760708649f7da0390cb5b8b5abaad845276afa
2Blagden <benblagden0@gmail.com>
2019-09-18 14:45:31 +0100

    commit

--- directory.rb
+++ directory.rb
@@ -1,18 +1,29 @@
 student_count = 11
+
+
 # first, we print the list of students
-puts "The students of villain's academy"
+students = [
+"The students of villain's academy"
+"____________"
+"Dr. Hannibal Lecter"
+"Darth Vader"
+"Nurse Ratched"
+"Michael Corleone"
+"Alex Delarge"
+"The Wicked Witch of the West"
+"Terminator"
+"Freddy Krueger"
+"The Joker"
+"Joffrey Baratheon"
+"Norman Bates"
+]
+
+puts "The students of Villains Academy"
 puts "____________"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex Delarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end
+
 #finally, we print the total number of students
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
 # it's important that prince() doesn't add new line character
