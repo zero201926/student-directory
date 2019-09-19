3406c0ca9ad69ce4cf097e38e05eda074452d824
vikash <zero_transfer@outlook.com>
2019-09-19 17:17:12 +0100

    step11

--- directory.rb
+++ directory.rb
@@ -88,6 +88,7 @@ end
 def print_menu
   puts "1. Input the Students"
   puts "2. Show the Students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -103,6 +104,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -117,4 +120,13 @@ def interatcive_menu
   end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 interatcive_menu()
