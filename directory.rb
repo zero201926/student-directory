782098f4de90adf86607dd1f528f712c29ca9769
vikash <zero_transfer@outlook.com>
2019-09-25 15:45:53 +0100

    step12

--- directory.rb
+++ directory.rb
@@ -89,6 +89,7 @@ def print_menu
   puts "1. Input the Students"
   puts "2. Show the Students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -106,6 +107,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -126,6 +129,15 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
